4c75c5d7294d9780addcc188538e33e7efcd3e7d
Ian De Jesus <dejesusigs@gmail.com>
2024-02-12 00:21:38 +0800

    yeeeee
    started progress wid userinfo

---
+++ JS/UserProfile.js
@@ -0,0 +1,7 @@
+function editPFP(){
+    document.getElementById('profile-picture').src = '../Media/PFP-Moon-Edit.jpg';
+}
+
+function backPFP(){
+    document.getElementById('profile-picture').src = '../Media/PFP-Moon.jpg';
+}
