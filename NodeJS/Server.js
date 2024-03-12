dd193593bee9e7a4e155b156ce6e72b84184d958
Neo <antonio_monserrat@dlsu.edu.ph>
2024-03-12 22:02:23 +0800

    Edits in server.js

--- NodeJS/Server.js
+++ NodeJS/Server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'CCAPDEV-MCO')));
-
+ 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../HTML/index.html"));
 });
