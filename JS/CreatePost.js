9d0160c89861ca443c7217d1e3ff256a1b7032ad
Neo <antonio_monserrat@dlsu.edu.ph>
2024-03-12 11:35:11 +0800

    edits

--- JS/CreatePost.js
+++ JS/CreatePost.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
-    // Your post data
     var postData = {
         title: "New Post Title",
         category: "General Discussions",
         body: "This is the body of the new post."
     };
 
-    // Create the HTML for the new post
+
     var newPost = `
         <div class="gen-discussion">
             <h3><a href="#">${postData.title}</a></h3>
@@ -14,6 +13,5 @@ $(document).ready(function() {
         </div>
     `;
 
-    // Append the new post to the "General Discussions" section
     $(".forum-section:contains('General Discussions')").append(newPost);
 });
