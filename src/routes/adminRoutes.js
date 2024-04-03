d3d989103c4a684724860ccc499e9366679117bb
Neo <antonio_monserrat@dlsu.edu.ph>
2024-04-04 07:38:35 +0800

    ...

---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+   
+});
+
+router.delete('/UpdateForums', async (req, res) => {
+   
+});
+
+
+
+module.exports = router;
